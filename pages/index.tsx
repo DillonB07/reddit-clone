36272b8863358efd3efeb47a5cff6bdf2a1fde7a
DillonB07 <dillonbarnes07@gmail.com>
2022-07-02 20:10:26 +0100

    Add PostBox

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,18 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import PostBox from "../components/PostBox";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="max-w-5xl my-7 mx-auto">
       <Head>
         <title>Reddit 2.0</title>
       </Head>
+
+      <div>
+        <PostBox />
+      </div>
     </div>
   );
 };
