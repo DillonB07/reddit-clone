7c38e859137ec0b353c3dd08582533edd1669069
DillonB07 <dillonbarnes07@gmail.com>
2022-07-02 17:28:55 +0100

    Add nextauth

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,8 +14,11 @@ import {
   MenuIcon,
   SearchIcon,
 } from "@heroicons/react/solid";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function Header() {
+  const { data: session } = useSession();
+
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="h-10 w-20 relative flex-shrink-0 cursor-pointer">
@@ -54,6 +57,43 @@ function Header() {
       <div className="ml-5 flex items-center lg:hidden">
         <MenuIcon className="icon" />
       </div>
+
+      {session ? (
+        <div
+          onClick={() => signOut()}
+          className="hidden lg:flex items-center space-x-2 border-gray-100 p-2 cursor-pointer"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              objectFit="contain"
+              src="https://links.papareact.com/23l"
+              layout="fill"
+              alt="Log in/out"
+            />
+          </div>
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session?.user?.name}</p>
+            <p className="text-gray-400">Sign Out</p>
+          </div>
+
+          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400" />
+        </div>
+      ) : (
+        <div
+          onClick={() => signIn()}
+          className="hidden lg:flex items-center space-x-2 border-gray-100 p-2 cursor-pointer"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              objectFit="contain"
+              src="https://links.papareact.com/23l"
+              layout="fill"
+              alt="Log in/out"
+            />
+          </div>
+          <p className="text-gray-400">Sign In</p>
+        </div>
+      )}
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,6 @@ const Home: NextPage = () => {
       <Head>
         <title>Reddit 2.0</title>
       </Head>
-
-      <Header />
     </div>
   );
 };
