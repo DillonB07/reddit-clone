c9bf4ac76cf9630758e8efc6f2a4004c773feba6
DillonB07 <dillonbarnes07@gmail.com>
2022-07-10 17:23:49 +0100

    Fix comment button

--- components/Post.tsx
+++ components/Post.tsx
@@ -220,6 +220,7 @@ const Post = ({ post, comments }: Props) => {
                     }
                   />
                   <button
+                    disabled={!session}
                     type="submit"
                     className="rounded-full bg-red-500 p-3 font-semibold text-white disabled:bg-gray-200"
                   >
